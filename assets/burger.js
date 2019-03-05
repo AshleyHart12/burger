172bda5e1241a04ce5b45911c6d5c7ef36d61f27
Ashinator <Ahart425@gmail.com>
2019-03-04 21:14:37 -0700

    polished functions

--- assets/burger.js
+++ assets/burger.js
@@ -1,20 +1,48 @@
 $(function() {
-    $("submit").on("click", function(event) {
-        var id = $(this).data("id");
-        var devoured = $(this)data("devoured");
 
-        var devourBurger = {
-            devoured: devoured
+    // ADD NEW BURGER
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+            devoured: 0
         };
-
-        $.ajax("/api/burgers" + id, {
-            type: "PUT",
-            data: devourBurger
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
         }).then(function() {
-            console.log("Burger Devoured", devoured);
+            console.log("Added New Burger");
             location.reload();
         });
     });
 
+    $(".eatburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredBurger = {
+            devoured: 1
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredBurger
+        }).then(function() {
+            console.log("Burger Devoured");
+            location.reload;
+        });
+    });
+
+    $(".trashburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(location.reload());
+    });
+  
 
 });
